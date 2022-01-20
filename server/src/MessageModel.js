834ba45beb559a2dc8837c04c3cfd314de711202
SophieTopart <sophiebailly9@gmail.com>
2022-01-20 11:29:52 +0100

    Test new PR

--- server/src/MessageModel.js
+++ server/src/MessageModel.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
 const MessageSchema = new Schema({
-  sentence: String
-});
-module.exports = mongoose.model("message", MessageSchema);
+	sentence: String,
+})
+module.exports = mongoose.model('message', MessageSchema)
